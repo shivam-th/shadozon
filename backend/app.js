734416521edd575787b519364416964fb979edc5
shivam-th <shivamthakur0567@gmail.com>
2023-12-23 16:25:15 +0530

    MongoDB Setup

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js";
+import connectDB from "./config/db.js";
 
+connectDB();
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
