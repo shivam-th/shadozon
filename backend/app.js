444669b1160fb52b29651584d65e398361612b26
shivam-th <shivamthakur0567@gmail.com>
2023-12-27 01:06:02 +0530

    Get Product from Database

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
+
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
 connectDB();
 dotenv.config();
@@ -12,13 +13,6 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
