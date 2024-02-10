e94be448d58b0c5e52c538c3198bcc8cb7af4418
shivam-th <shivamthakur0567@gmail.com>
2024-02-10 05:42:02 +0530

    Login Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+<ToastContainer />;
 
 const App = () => {
   return (
